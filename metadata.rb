eef6dd90b973dede4be4a6b8533e7dc74d9248ba
Jake Davis <jake.davis5989@gmail.com>
2013-07-31 16:59:07 +0000

    Small version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ description           'Installs the Go language'
 maintainer            'Simple Finance'
 maintainer_email      'ops@simple.com'
 license               'Apache 2.0'
-version               '1.0.0'
+version               '1.0.1'
 
 supports 'ubuntu'
 
